e0fdf98e3b9c73f0a49eb8ab758ba2d3780b6ee7
Borislav Borisov <borislav.borisov32@gmail.com>
2018-09-03 23:46:12 +0300

    add js files for client side

---
+++ js/viewAvgAndMaxPaymentsPeriod.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+
+    $('#datepicker').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
+    $('#datepicker2').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
+  });
